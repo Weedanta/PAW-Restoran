a3293a0ff1737949319501d3a6e785194c992124
Weedanta <baguswedanta17@gmail.com>
2024-11-21 00:06:32 +0700

    Footer

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -13,23 +13,37 @@ function Home({ name }) {
                     LALAL
                 </article>
                 <figure className="h-full">
-                    <img src="/images/Menu1.png" alt="" className="h-full"/>
+                    <img src="/images/Menu1.png" alt="" className="h-full" />
                 </figure>
             </section>
             <section className="h-screen">
 
             </section>
             <footer className="h-[50vh] bg-primary-1 px-28 py-16">
-                <article className="h-full flex flex-row">
-                    <div className="w-[32.3%] h-full flex flex-col text-white">
-                        <h1>LOGO</h1>
-                        <h3>Urban Feast is a place where bold flavors, fresh ingredients, and creative dishes come together. Join us for a dining experience that celebrates the art of food in a welcoming atmosphere.</h3>
+                <article className="h-full flex flex-row gap-10">
+                    <div className="w-[40%] h-full flex flex-col text-white gap-4">
+                        <img src="/images/Logo.png" alt="" className="h-28 w-28" />
+                        <h3 className="font-normal text-2xl text-justify">Urban Feast is a place where bold flavors, fresh ingredients, and creative dishes come together. Join us for a dining experience that celebrates the art of food in a welcoming atmosphere.</h3>
+                        <p className="mt-3">©UrbanFeast</p>
                     </div>
                     <div className="w-[0.05%] h-full bg-white opacity-30"></div>
-                    <div className="w-[32.3%] h-full"></div>
+                    <div className="w-[35%] h-full text-white">
+                        <div className="">
+                            <h3 className="text-2xl">Phone</h3>
+                            <p className="text-xl">+62-8759 - 920 - 101</p>
+                        </div>
+                        <div className="">
+                            <h3 className="text-2xl">Email</h3>
+                            <p className="text-xl">urban_feast@gmail.com</p>
+                        </div>
+                        <div className="">
+                            <h3 className="text-2xl">Alamat</h3>
+                            <p className="text-xl">Cosco Street Avenue, Jl. Pantai Indah Kapuk 2, Kapuk, Jakarta Utara.</p>
+                        </div>
+                    </div>
                     <div className="w-[0.05%] h-full bg-white opacity-30"></div>
-                    <div className="w-[32.3%] h-full"></div>
-                    
+                    <div className="w-[24%] h-full"></div>
+
                 </article>
             </footer>
         </main>
