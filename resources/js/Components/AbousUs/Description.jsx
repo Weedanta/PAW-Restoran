4c1c7a442c1f08be8104b399b1b825a3ff495715
Weedanta <baguswedanta17@gmail.com>
2024-11-29 23:32:20 +0700

    Description responsive

--- resources/js/Components/AbousUs/Description.jsx
+++ resources/js/Components/AbousUs/Description.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Description = () => {
     return (
         <>
-            <section className='h-screen bg-primary-3 w-full md:px-10 flex items-center '>
-                <div className="h-[72%] w-full bg-primary-1 flex justify-between px-16 items-center rounded-3xl text-white gap-12">
+            <section className='h-full md:h-screen bg-primary-3 w-full px-4 md:px-10 flex items-center justify-center py-12 md:pt-0'>
+                <div className="h-full  md:h-[72%] w-full bg-primary-1 flex flex-col md:flex-row justify-between px-6 md:px-10 lg:px-16 items-center rounded-3xl text-white gap-12 pt-12 md:pt-0 pb-6 md:pb-0">
                     <article className=''>
-                        <p className='font-medium text-3xl text-justify'>Founded in 2015, Urban Feast was born out of a passion for bringing together bold flavors and fresh ingredients in a modern, welcoming atmosphere. Our mission is simple: to offer a dining experience that celebrates creativity, quality, and the joy of great food. From our carefully crafted dishes to the vibrant ambiance, every meal is an invitation to savor something truly special.</p>
+                        <p className='font-medium md:text-lg lg:text-3xl text-justify'>Founded in 2015, Urban Feast was born out of a passion for bringing together bold flavors and fresh ingredients in a modern, welcoming atmosphere. Our mission is simple: to offer a dining experience that celebrates creativity, quality, and the joy of great food. From our carefully crafted dishes to the vibrant ambiance, every meal is an invitation to savor something truly special.</p>
                     </article>
-                    <img src="/images/aboutUs/description.png" alt="" className="object-cover h-[120%]" draggable="false" />
+                    <img src="/images/aboutUs/description.png" alt="" className="object-cover h-full md:h-[120%]" draggable="false" />
                 </div>
                 .
             </section>
