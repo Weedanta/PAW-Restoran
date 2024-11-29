2f1987ab746c99df3ba8084b44d11dc86bc7c06f
Weedanta <baguswedanta17@gmail.com>
2024-11-30 01:18:32 +0700

    Contact Us

--- resources/js/Pages/ContactUs.jsx
+++ resources/js/Pages/ContactUs.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <section className='h-screen bg-primary-3'>
-
+    <section className='h-screen bg-primary-3 px-24 py-12'>
+        <div className="h-full w-full bg-black fle"></div>
     </section>
   )
 }
