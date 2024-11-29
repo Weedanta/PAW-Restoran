871f75282492755346a42774ea81328a5f2fbe59
Weedanta <baguswedanta17@gmail.com>
2024-11-29 23:19:00 +0700

    Description non responsive

---
+++ resources/js/Components/AbousUs/Description.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Description = () => {
+    return (
+        <>
+            <section className='h-screen bg-primary-3 w-full md:px-10 flex items-center '>
+                <div className="h-[72%] w-full bg-primary-1 flex justify-between px-16 items-center rounded-3xl text-white gap-12">
+                    <article className=''>
+                        <p className='font-medium text-3xl text-justify'>Founded in 2015, Urban Feast was born out of a passion for bringing together bold flavors and fresh ingredients in a modern, welcoming atmosphere. Our mission is simple: to offer a dining experience that celebrates creativity, quality, and the joy of great food. From our carefully crafted dishes to the vibrant ambiance, every meal is an invitation to savor something truly special.</p>
+                    </article>
+                    <img src="/images/aboutUs/description.png" alt="" className="object-cover h-[120%]" draggable="false" />
+                </div>
+                .
+            </section>
+        </>
+    )
+}
+
+export default Description
--- resources/js/Pages/AboutUs.jsx
+++ resources/js/Pages/AboutUs.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Hero from '../Components/LandingPage/Hero'
+import Description from '../Components/AbousUs/Description'
+
 
 const AboutUs = () => {
   return (
-    <div>
+    <main>
+      <Hero image={"/images/Reservation/background.png"}>
+        <h1 className="text-3xl md:text-5xl font-semibold">
+          Uncover the Essence of
+        </h1>
+        <h1 className="text-3xl md:text-5xl font-semibold mb-8 md:mb-2">
+          Who We Truly Are and What Sets Us Apart.
+        </h1>
+      </Hero>
+      <Description/>
       
-    </div>
+
+    </main>
   )
 }
 
