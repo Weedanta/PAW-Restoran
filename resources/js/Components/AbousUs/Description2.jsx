1877c37baa4b9d8d843b2c3edf83af65aeec75c7
Weedanta <baguswedanta17@gmail.com>
2024-11-30 00:47:50 +0700

    Description2 responsive

--- resources/js/Components/AbousUs/Description2.jsx
+++ resources/js/Components/AbousUs/Description2.jsx
@@ -3,9 +3,9 @@ import Button from '../LandingPage/Button'
 
 const Description2 = () => {
     return (
-        <section className='h-full md:h-screen w-full md:p-28 p-4 bg-primary-2 flex   items-center' >
+        <section className='h-full md:h-screen w-full md:p-28 px-4 py-6 bg-primary-2 flex   items-center' >
             <div className="w-full flex justify-between items-center gap-4 md:gap-10 lg:gap-16 flex-col-reverse md:flex-row">
-                <article className='text-white flex flex-col text-center md:text-justify gap-6 md:gap-10 px-10 md:px-0'>
+                <article className='text-white flex flex-col text-center md:text-justify gap-6 md:gap-10 px-6 md:px-0'>
                     <h1 className='text-4xl font-semibold'>A Taste of Our Journey!</h1>
                     <p className='text-xl md:text-2xl'>Since 2015, we’ve been crafting unforgettable dishes that blend tradition with innovation. Our menu reflects the journey of Urban Feast, featuring flavors that have evolved over the years while staying true to our commitment to fresh, high-quality ingredients. Explore our menu and taste the story behind every dish.</p>
                     <Button 
