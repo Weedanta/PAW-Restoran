d3f79fcf61753c38dc8b29c2cdfa119670d8f22b
Weedanta <baguswedanta17@gmail.com>
2024-12-01 15:25:51 +0700

    Menu Controller

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class MenuController extends Controller
 {
-    //
+    public function index()
+    {
+        $menu = Menu::orderBy('kategori')
+            ->orderBy('nama_menu')
+            ->get()
+            ->groupBy('kategori');
+
+        return Inertia::render('Menu/Index', [
+            'menu' => $menu
+        ]);
+    }
 }
